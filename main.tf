13b60687cdaace7ee24504041a65511a8ee04ff7
UdbhasB <baraludvash22@gmail.com>
2025-06-08 16:54:07 +0545

    changes in acl

--- main.tf
+++ main.tf
@@ -5,24 +5,11 @@ provider "aws" {
 
 # Resource to create an S3 bucket. Stores the terraform state files.
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "UDBHAS-TERRAFORM-PROJECT" # Should be globally unique
-  acl    = "private" # Set the ACL to private for security
-
-  versioning {
-    enabled = true
+  bucket = "udbhas-terraform-state-20250607"
+  tags = {
+    Name        = "UDBHAS-TERRAFORM-PROJECT"
+    Environment = "Production"
   }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256" # Use AES256 encryption
-      }
-    }
-  }
-    tags = {
-        Name        = "UDBHAS-TERRAFORM-PROJECT"
-        Environment = "Production"
-    }
 }
 
  # Block all public access to the S3 bucket.
@@ -33,6 +20,14 @@ resource "aws_s3_bucket_public_access_block" "terraform_state_publinc_access_blo
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+# Resource to enable versioning on the S3 bucket.
+resource "aws_s3_bucket_versioning" "terraform_state_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 # Resource to create a DynamoDB table for state locking. so that multiple users can work on the same state file without conflicts.
